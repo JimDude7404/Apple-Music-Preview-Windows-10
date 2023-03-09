79556687e0cdc895c16688e394a6d318bc291ecd
JimDude <67085206+JimDude7404@users.noreply.github.com>
2023-03-08 21:00:17 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -14,17 +14,21 @@ echo Copying data to new folder...
 set "source=%~dp0AppleMusic"
 set "dest=%userprofile%\Documents\AppleMusicClient"
 xcopy "%source%" "%dest%" /s /i /q
-
+:: Kills all UWP apps
+taskkill /F /IM ApplicationFrameHost.exe >nul 2>&1
+:: Kills Gaming Service, it likes to get in the way
+powershell -Command "Stop-Service -Name GamingServices"
 :: Register AppxManifest.xml
 echo Registering AppxManifest.xml...
 powershell -Command "Add-AppxPackage -Register '%userprofile%\Documents\AppleMusicClient\AppxManifest.xml'"
-
+taskkill /F /IM ApplicationFrameHost.exe >nul 2>&1
 :: Install dependencies
 echo Installing dependencies...
 for %%f in ("%~dp0*.appx") do (
     echo Installing %%f...
     powershell -Command "Add-AppxPackage '%%~ff'"
 )
-
+taskkill /F /IM ApplicationFrameHost.exe >nul 2>&1
 :: Done!
 echo Apple Music has been installed!
+pause
